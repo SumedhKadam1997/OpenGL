a2be11eab23132f294f86e00d4c75990043f9d72
Verolt - Sumedh <sumedh.kadam@verolt.com>
2021-09-02 09:00:21 +0530

    Added Textures Exercise

--- Project3 - Shape Transform/assets/fragment_core2.glsl
+++ Project3 - Shape Transform/assets/fragment_core2.glsl
@@ -3,6 +3,6 @@ out vec4 FragColor;
 in vec4 color;
 
 void main() {
-FragColor = vec4(0.1f, 0.8f, 0.2f, 1.0f);
-	//FragColor = color;
+//FragColor = vec4(0.1f, 0.8f, 0.2f, 1.0f);
+	FragColor = color;
 }
